29a6f6a303eb3f231a9868871c2806a9234ad18c
=Manuel Valdez <=me.valdez@dga.gov.do>
2018-05-10 14:47:08 -0400

    cleanup entities

--- adconcilio/eventos/admin.py
+++ adconcilio/eventos/admin.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Evento, EventoMaterial, Promocion, FechaEvento, Exposicion, AgentePago, Localidad, Expositor, Recibo, Registro, Participante
+from .models import Evento, Material, Promocion, Exposicion, Localidad, Expositor, Registro, Participante
 
 admin.site.register(Evento)
-admin.site.register(EventoMaterial)
+admin.site.register(Material)
 admin.site.register(Promocion)
-admin.site.register(FechaEvento)
 admin.site.register(Exposicion)
-admin.site.register(AgentePago)
 admin.site.register(Localidad)
 admin.site.register(Expositor)
-admin.site.register(Recibo)
 admin.site.register(Registro)
 admin.site.register(Participante)
--- adconcilio/eventos/models.py
+++ adconcilio/eventos/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from datetime import datetime
-from datetime import deltatime
-from datetime import time
 
 # Create your models here.
 from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 #import uuid
+from datetime import datetime
+from datetime import  timedelta
+from datetime import time
 
 class Evento(models.Model):
 
@@ -13,58 +13,58 @@ class Evento(models.Model):
     descripcion = models.TextField(max_length=256)
     tarifa = models.DecimalField(max_digits=8, decimal_places=2)
     duracion = models.TimeField(default=datetime.now().replace(hour=4, minute=0, second=0, microsecond=0), blank=True)
-	#fecha_efectiva = models.DateField(default=datetime.datetime.strptime("9999-31-12", "%Y-%m-%d").date())
-	fecha_efectiva = models.DateField(default=datetime.datetime.date(9999, 12, 31))
-	EVENT_ESTADO = (
+    #fecha_efectiva = models.DateField(default=datetime.strptime("9999-12-31", "%Y-%m-%d").date())
+    fecha_efectiva = models.DateField(default=datetime(9999, 12, 31))
+    EVENT_ESTADO = (
         ('a', 'activo'),
         ('i', 'inactivo'),
     )
-	models.CharField(max_length=1, choices=EVENT_ESTADO, blank=True, default='a' )
-       
+    models.CharField(max_length=1, choices=EVENT_ESTADO, blank=True, default='a' )
+	
     class Meta:
         ordering = ["nombre"]
-		
+        
     def __str__(self):
 
         return self.nombre   
-	
+    
 class Material(models.Model):
 
     evento = models.ForeignKey('Evento', on_delete=models.SET_NULL, null=True)
-	libro_estudio = models.TextField(max_length=80)
-	guia_estudio =  models.TextField(max_length=80)
-	MULT_MEDIA = (
+    libro_estudio = models.TextField(max_length=80)
+    guia_estudio =  models.TextField(max_length=80)
+    MULT_MEDIA = (
         ('d', 'DVD'),
         ('c', 'CD'),
-		('u', 'USB'),
-		('k', 'Disco'),
-		('o', 'Otro'),        
+        ('u', 'USB'),
+        ('k', 'Disco'),
+        ('o', 'Otro'),        
     )
-	multi_medios = models.CharField(max_length=1, choices=MULT_MEDIA, blank=True, default='o' )
+    multi_medios = models.CharField(max_length=1, choices=MULT_MEDIA, blank=True, default='o' )
  
-   class Meta:
-        ordering = ["Evento"]
-		verbose_name="Material"
-		verbose_name_plural="Materiales"
-		
+    class Meta:
+        ordering = ["evento"]
+        verbose_name="Material"
+        verbose_name_plural="Materiales"
+        
     def __str__(self):
 
         return self.evento.nombre   
 
-		
+        
 class Promocion(models.Model):
 
     nombre_ciclo = models.ForeignKey('Ciclo', on_delete=models.SET_NULL, null=True)
-	nombre_programa = models.ForeignKey('Programa', on_delete=models.SET_NULL, null=True)
-	evento = models.ForeignKey('Evento', on_delete=models.SET_NULL, null=True)
+    nombre_programa = models.ForeignKey('Programa', on_delete=models.SET_NULL, null=True)
+    evento = models.ForeignKey('Evento', on_delete=models.SET_NULL, null=True)
     fecha_inicio = models.DateField(default=datetime.today)
-    fecha_fin = models.DateField(default=datetime.today + timedelta(days=30))
+    fecha_fin = models.DateField(default = datetime.today)
     imagen = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
 
     class Meta:
         ordering = ["fecha_inicio"]
-		verbose_name = "Promoci\u00F3n"
-		verbose_name_plural = "Promociones"
+        verbose_name = "Promoci\u00F3n"
+        verbose_name_plural = "Promociones"
         
     def __str__(self):
     
@@ -96,11 +96,11 @@ class Exposicion(models.Model):
     fecha_inicio = models.DateField(default=datetime.today, blank=True)
     hora_inicio = models.TimeField(default=time(10, 0, 0, 0), blank=True)
     hora_final = models.TimeField(default=time(12, 0, 0, 0), blank=True)
-	
+    
     class Meta:
         ordering = ["fecha_inicio"]
-		verbose_name = "Exposici\u00F3n"
-		verbose_name_plural = "Exposiciones"
+        verbose_name = "Exposici\u00F3n"
+        verbose_name_plural = "Exposiciones"
 
             
     def __str__(self):
@@ -122,11 +122,11 @@ class Expositor(models.Model):
     )
     estado = models.CharField(max_length=1, choices=EXPO_ESTADO, blank=True, default='a')
     fecha = fecha = models.DateField(default=datetime.today, editable=False)
-	
+    
     class Meta:
         ordering = ["apellido", "nombre"]
-		verbose_name = "Expositor"
-		verbose_name_plural = "Expositores"
+        verbose_name = "Expositor"
+        verbose_name_plural = "Expositores"
 
     def __str__(self):
     
@@ -137,14 +137,14 @@ class Localidad(models.Model):
         
     nombre = models.CharField(max_length=50)
     edificio = models.CharField(max_length=50)
-	direccion = models.CharField(max_length=120)
+    direccion = models.CharField(max_length=120)
     salon = models.CharField(max_length=50)
     capacidad = models.IntegerField(default=0 )
-	
+    
     class Meta:
         ordering = ["direccion"]
-		verbose_name = "Localidad"
-		verbose_name_plural = "Localidades"
+        verbose_name = "Localidad"
+        verbose_name_plural = "Localidades"
             
     def __str__(self):
     
@@ -158,11 +158,11 @@ class Participante(models.Model):
     direccion = models.CharField(max_length=120)
     correo = models.EmailField()
     telefono = models.CharField(max_length=10)
-	fecha = models.DateField(default=datetime.today, editable=False)
-	
+    fecha = models.DateField(default=datetime.today, editable=False)
+    
     class Meta:
         ordering = ["apellido", "nombre"]
-		
+        
     def __str__(self):
     
         return '{0}, {1}'.format(self.apellido, self.nombre)
@@ -170,12 +170,13 @@ class Participante(models.Model):
 class Registro(models.Model):
       
     evento = models.ForeignKey('Evento', on_delete=models.SET_NULL, null=True)
-	exposicion = models.ForeignKey('Exposicion', on_delete=models.SET_NULL, null=True)
+    exposicion = models.ForeignKey('Exposicion', on_delete=models.SET_NULL, null=True)
     participante  = models.ForeignKey('Participante', on_delete=models.SET_NULL, null=True)
     fecha = models.DateField(default=datetime.today, editable=False)
+    
     class Meta:
-        ordering = ['Evento', 'Participante']
-		
+        ordering = ['evento', 'participante']
+        
     def __str__(self):
     
         return '{0}, {1}'.format(self.evento.nombre, self.participante.nombre)
@@ -189,18 +190,18 @@ class Factura(models.Model):
     FACT_ESTADO = (
         ('p', 'pagada'),
         ('n', 'nueva'),
-		('c', 'cancelada'),
+        ('c', 'cancelada'),
     )
     estado = models.CharField(max_length=1, choices=FACT_ESTADO, blank=True, default='n')
     fecha = fecha = models.DateField(default=datetime.today, editable=False)
                      
     def __str__(self):
         return '{0}, {1}'.format(self.registro.participante.cedula, self.registro.participante.nombre)        
-		
+        
 class DetalleFactura(models.Model):
        
     factura = models.ForeignKey('Factura', on_delete=models.SET_NULL, null=True)
-	registro = models.ForeignKey('Registro', on_delete=models.SET_NULL, null=True)
+    registro = models.ForeignKey('Registro', on_delete=models.SET_NULL, null=True)
     referencia = models.CharField(max_length=50)
     nota = models.TextField(max_length=256)
     fecha = models.DateField(default=datetime.today, editable=False)
@@ -209,7 +210,7 @@ class DetalleFactura(models.Model):
                  
     def __str__(self):
         return '{0}, {1}'.format(self.registro.participante.cedula, self.registro.participante.nombre)        
-		
+        
 class Recibo(models.Model):
        
     Factura= models.ForeignKey('Factura', on_delete=models.SET_NULL, null=True)
