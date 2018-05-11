4128de228ac3859e3542f18cf1468a8ec9b08296
=Manuel Valdez <=me.valdez@dga.gov.do>
2018-05-11 08:59:51 -0400

    fixed gitignore

---
+++ adconcilio/eventos/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Evento, Material, Promocion, Exposicion, Localidad, Expositor, Registro, Participante, Ciclo, Programa
+
+admin.site.register(Evento)
+admin.site.register(Material)
+admin.site.register(Promocion)
+admin.site.register(Exposicion)
+admin.site.register(Localidad)
+admin.site.register(Expositor)
+admin.site.register(Registro)
+admin.site.register(Programa)
+admin.site.register(Ciclo)
+admin.site.register(Participante)
